1c3bb0deead24513c01b68d148b64df678340e29
Milos Bogdanovic <milos.bogdanovic@symphony.is>
2023-08-25 11:01:14 +0200

    adadsdasdasd
    Signed-off-by: Milos Bogdanovic <milos.bogdanovic@symphony.is>

--- SS/SS/ContentView.swift
+++ SS/SS/ContentView.swift
@@ -15,6 +15,7 @@ struct ContentView: View {
                 .foregroundColor(.accentColor)
             Text("ADDDDD")
             Text("ADDDDD")
+            Text("ADDDDDsadsda")
         }
         .padding()
     }
