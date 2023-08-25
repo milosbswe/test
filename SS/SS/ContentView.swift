37193b0e0edefa547772a0be8d0b58a0c8f22b37
Milos Bogdanovic <milos.bogdanovic@symphony.is>
2023-08-25 10:55:28 +0200

    initial commit

---
+++ SS/SS/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SS
+//
+//  Created by Milos Bogdanovic on 08/25/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SS/SS/SSApp.swift
@@ -0,0 +1,17 @@
+//
+//  SSApp.swift
+//  SS
+//
+//  Created by Milos Bogdanovic on 08/25/23.
+//
+
+import SwiftUI
+
+@main
+struct SSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
