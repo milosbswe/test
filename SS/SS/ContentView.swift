d86920b0c5d40a723a04c61d4d25525f30aab937
Milos Bogdanovic <milos.bogdanovic@symphony.is>
2024-02-16 17:20:46 +0100

    add1

--- SS/SS/ContentView.swift
+++ SS/SS/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("ADDDDD")
+            Text("1111")
         }
         .padding()
     }
